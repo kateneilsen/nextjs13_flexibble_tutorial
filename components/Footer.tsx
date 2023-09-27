86448408a484238a3a8a714e0d981b0ea669628e
Kate <neilsenk09@gmail.com>
2023-09-27 12:04:05 -0400

    start of footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flexStart footer">
+      <div className="flex flex-col gap-12"></div>
+    </footer>
+  );
 };
 
 export default Footer;
