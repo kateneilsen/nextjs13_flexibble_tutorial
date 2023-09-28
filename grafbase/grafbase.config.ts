4f497be450a4a6f6ff4541a80ddae3175266002a
Kate <neilsenk09@gmail.com>
2023-09-28 16:32:41 -0400

    added to authentication

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,6 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+// @ts-ignore
 const User = g.model('User', {
   name: g.string().length({min: 2, max: 20}),
   email: g.string().unique(),
@@ -8,8 +9,11 @@ const User = g.model('User', {
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
 })
 
+// @ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({min: 3}),
   description: g.string(),
