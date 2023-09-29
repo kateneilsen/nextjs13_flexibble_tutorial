48b1fb062e0451e1a410cc0e83d9bf675f3290f8
Kate <neilsenk09@gmail.com>
2023-09-29 11:22:41 -0400

    updated create / get user

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,7 +2,7 @@ import { createUserMutation, getUserQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === 'production';
-const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:5000/graphql';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 
@@ -17,16 +17,18 @@ const makeGraphQLRequest = async(query: string, variables = {}) => {
 }
 
 export const getUser = (email: string) => {
+    client.setHeader('x-api-key', apiKey)
    return makeGraphQLRequest(getUserQuery, {email})
 }
 
 export const createUser = (name: string, email: string, avatarUrl: string) => {
+    client.setHeader('x-api-key', apiKey)
     const variables = {
         input: {
-            name, email, avatarUrl
-        }
+            name: name,
+            email: email,
+            avatarUrl: avatarUrl
+          },
     }
-    return makeGraphQLRequest(createUserMutation, {
-        variables
-    })
+    return makeGraphQLRequest(createUserMutation, variables)
 }
